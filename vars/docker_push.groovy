f62a2f18028fc73c76ade2aa33b5eaa632687f61
Siddhesh Pansare <61089801+Siddhesh-hub@users.noreply.github.com>
2026-02-04 18:10:05 +0530

    Add docker_push function to push images to Docker Hub
    This function logs into Docker Hub and pushes a Docker image.

---
+++ vars/docker_push.groovy
@@ -0,0 +1,11 @@
+def call(String ImageName, String ImageTag, String dockerHubUser){
+  echo "Pushing the image to Docker Hub"
+  withCredentials([usernamePassword(
+                    credentialsId:"docker-hub-cred", 
+                    passwordVariable:"dockerHubPass", 
+                    usernameVariable:"dockerHubUser")
+                    ]){
+  sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+  sh "docker image tag ${ImageName}:${ImageTag} ${env.dockerHubUser}/${ImageName}:${ImageTag}"
+  sh "docker push ${env.dockerHubUser}/${ImageName}:${ImageTag}"
+}
